63e1a81b936a954c52e797e5743c98a9e2ddd8d1
Isaiah Sinclair <isaiah.sinclair.0711@gmail.com>
2023-06-25 20:22:44 -0400

    Adjusted directory structure for front-end.

--- backend/expenseTracker/models.py
+++ backend/expenseTracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Expense(models.Model): # FIXME
+class Expense(models.Model):
     name = models.CharField(max_length=120, default="")
     amount = models.DecimalField(max_digits=20, decimal_places=4, default=0)
     date = models.DateField(default="2022-12-27")
